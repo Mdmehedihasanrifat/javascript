950dec8ffa3a8b6baaaf6c8bce65ddd9f1d20268
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2020-05-09 17:51:28 +0600

    day1 about variable

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// // primativetype
+//
+// //String="sum of charc"
+// var a;
+// console.log("a");
+//
+// //undefined
+//
+//
+// let b;
+// console.log(b);
+//
+// //undefined
+//
+//
+//
+// const c;
+// //SyntaxError: missing = in const declaration[Learn More]
+//
+
+//
+// example 2
+//challenge 1;
+
+
+
+
+
+
+let x=10;
+let y=true;
+
+const myObject={x,y};
+console.log(myObject);
+let myAnotherObject;
+  myAnotherObject={nameA:20,
+  b:true,
+  c:myObject
+};
+console.log(myAnotherObject);
+
